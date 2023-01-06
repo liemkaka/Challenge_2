ae082821665d0f421d34d85b0a3b6dbf5340577c
liemkaka <nc296962@gmail.com>
2023-01-07 04:18:11 +0700

    5th commit

--- main.js
+++ main.js
@@ -60,6 +60,29 @@ function closeButton(){
   var vid = document.getElementById("video");
   vid.play();
 }
+// Scroll to the top
+let mybutton = document.getElementById("btnTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    // mybutton.style.display = "block";
+    mybutton.style.visibility = "visible";
+    mybutton.style.animation = "Thuphong 0.5s";
+  } else {
+    mybutton.style.animation = "Thuphong-2 0.5s";
+    // mybutton.style.display = "none";
+    mybutton.style.visibility = "hidden";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 
 
